0dfbd1be93b52596c2e6521b83bea8cb40b743df
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-03 09:16:10 -0800

    dynamuc values

---
+++ Types/dynamic.js
@@ -0,0 +1,9 @@
+"use strict";
+// using any type 
+let ageNumber = '100';
+ageNumber = 100;
+ageNumber = {
+    years: 100,
+    months: 2,
+};
+console.log(ageNumber);
---
+++ Types/dynamic.ts
@@ -0,0 +1,9 @@
+// using any type 
+let ageNumber: any = '100';
+ageNumber = 100;
+ageNumber = {
+  years: 100,
+  months: 2,
+};
+
+console.log(ageNumber);
