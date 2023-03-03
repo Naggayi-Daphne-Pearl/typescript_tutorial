a4d8eef0385d8e23b2024ec2e826931f85177c3a
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-03 07:55:26 -0800

    setting the variable by adding the type

---
+++ Types/type.js
@@ -0,0 +1,12 @@
+"use strict";
+// Setting the type of the variables. 
+let identification = 5;
+let firstname = 'danny';
+let hasDog = true;
+let unit; // Declare variable without assigning a value
+unit = 5;
+// Union type 
+//  A union type is a variable that can be assigned more than one type:
+let age;
+age = 26;
+age = '26';
---
+++ Types/type.ts
@@ -0,0 +1,14 @@
+// Setting the type of the variables. 
+
+let identification: number = 5;
+let firstname: string = 'danny';
+let hasDog: boolean = true;
+
+let unit: number; // Declare variable without assigning a value
+unit = 5;
+
+// Union type 
+//  A union type is a variable that can be assigned more than one type:
+let age: string | number;
+age = 26;
+age = '26'
