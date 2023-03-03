a055b3c8ed9842ebd91fd2793df07745c70cce8f
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-03 07:54:27 -0800

    PRIMITIVE TYPES

---
+++ Types/primitive.ts
@@ -0,0 +1,11 @@
+// strings are immutable and cannot be altered
+let list = 'Danny';
+// name.toLowerCase();
+// console.log(name); // Danny - the string method didn't mutate the string
+
+//arrays are mutable 
+let arr = [1, 3, 5, 7];
+arr.pop();
+console.log(arr); // [1, 3, 5] - the array method mutated the array
+
+// name = 'Anna' // Assignment gives the primitive a new (not a mutated) value
