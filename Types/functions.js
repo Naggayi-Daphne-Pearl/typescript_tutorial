5e8ddc421ba3e7419e9dc5790b5eef71c9ece838
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-03-03 09:10:14 -0800

    functions in typeScript

---
+++ Types/functions.js
@@ -0,0 +1,21 @@
+"use strict";
+// Functions
+// We can define what the types the function arguments should be, as well as the return type of the function
+// Define a function called circle that takes a diam variable of type number, and returns a string
+function circle(diam) {
+    return "The circumference is " + Math.PI * diam;
+}
+console.log(circle(10)); // The circumference is 31.41592653589793
+// ES6 arrow function 
+/*const circle = (diam: number): string => {
+    return 'The circumference is ' + Math.PI * diam;
+  };
+  
+  console.log(circle(10)); // The circumference is 31.41592653589793*/
+// type: void 
+function sayHello(name) {
+    console.log('Hello ' + name);
+}
+sayHello('Daphne');
+let foo;
+console.log(foo); // returns undefined
